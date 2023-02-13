5932625ecff4a81480f23b53df66010f069a266d
DmitriT143 <124970131+DmitriT143@users.noreply.github.com>
2023-02-13 21:32:57 +0300

    Update Sqare X.py

--- Sqare X.py
+++ Sqare X.py
@@ -2,9 +2,9 @@
 # А нужно найти Корни x#
 
 def X_from_sqare():
-	a = 1
-	b = 4
-	c = -4
+	a = float(input("a = "))
+	b = float(input("b = "))
+	c = float(input("c = "))
 	Disk = (b ** 2) + 4 * a * c
 	print('a = ',a,', b = ',b,', c = ',c,', d = ',Disk)
 	if Disk < 0:
@@ -15,7 +15,8 @@ def X_from_sqare():
 		print('x= ',x)
 	elif Disk>0:
 		print('Perfect')
-		x = (((0 - b) + Disk)/a)
-		print('x= ',x)
+		x_1 = (((0 - b) + (Disk**(-1/2)))/a)
+		x_2 = (((0 - b) - (Disk**(-1/2)))/a)
+		print('x= ',x_1, " or x = ", x_2)
 
 X_from_sqare()
